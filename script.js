08032b5a31c0a6560c09267c0eb971cde48cd468
Cleciano <clecianoff@gmail.com>
2023-09-08 09:31:44 -0300

    Condição script

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ function raffle() {
     const minValue = +(document.querySelector("#min-value").value);
     const maxValue = +(document.querySelector("#max-value").value);
 
-    result.innerHTML = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    if(maxValue >= minValue){
+        result.innerHTML = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+    }
+    else{
+        alert("Atenção!!! O ÚLTIMO VALOR é MENOR que o PRIMEIRO!!!")
+    }
 }
 
 raffleButton.addEventListener("click", raffle)
